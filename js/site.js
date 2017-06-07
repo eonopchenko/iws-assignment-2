630bf07c3d6ab065530db92b65a9c7a3246a1682
Alexandr Li <alexli0612@gmail.com>
2017-06-07 18:50:18 +1200

    About Page
    Created Logo, added contact info

--- js/site.js
+++ js/site.js
@@ -28,14 +28,28 @@ $(function(){
 // About page
 
 function initMap() {
-	var wsw = {lat: -36.853507, lng: 174.766542};
+	var wsw = {lat: -45.870823, lng: 170.504931};
 	var map = new google.maps.Map(document.getElementById("googleMap"), {
 		center: wsw,
 		zoom: 15
+	});
+	var contentString = '<div id="content">'+
+		'<div id="siteNotice">'+
+		'</div>'+
+		'<h5 id="firstHeading" class="firstHeading">Wall Street Wines</h5>'+
+		'<div id="bodyContent">'+
+		'<p>211 George St, Dunedin, 9016</p>'+
+		'</div>'+
+		'</div>';
+	var infowindow = new google.maps.InfoWindow({
+		content: contentString
 	});
 	var marker = new google.maps.Marker({
 	position: wsw,
 	map: map,
 	animation: google.maps.Animation.DROP
+	});
+	marker.addListener('click', function() {
+	infowindow.open(map, marker);
 	});
 }

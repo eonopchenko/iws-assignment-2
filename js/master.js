ce29fc77f7b31ce1db8f7eacbba3969abb22bb13
Alexandr Li <alexli0612@gmail.com>
2017-06-09 02:21:27 +1200

    About, Signup Pages
    Design, Header, Footer

--- js/master.js
+++ js/master.js
@@ -1,4 +1,4 @@
-// About page
+// About Page
 
 function initMap() {
 	var wsw = {lat: -45.870823, lng: 170.504931};
@@ -25,4 +25,47 @@ function initMap() {
 	marker.addListener('click', function() {
 	infowindow.open(map, marker);
 	});
+}
+
+// Sign Up Page
+function onClickSubmit() {
+  var validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (document.getElementById("InputName").value.length < 5) {
+    document.getElementById("groupName").className = "form-group has-error";
+    document.getElementById("errorName").innerHTML = "Name must be at least 5 letters";
+  }
+  else {
+    document.getElementById("groupName").className = "form-group has-success";
+    document.getElementById("errorName").innerHTML = "";
+  }
+  if (document.getElementById("InputSurname").value.length < 8) {
+    document.getElementById("groupSurname").className = "form-group has-error";
+    document.getElementById("errorSurname").innerHTML = "Surname must be at least 8 letters";
+  }
+  else {
+    document.getElementById("groupSurname").className = "form-group has-success";
+    document.getElementById("errorSurname").innerHTML = "";
+  }
+  if (validEmail.test(document.getElementById("InputEmail").value)) {
+    document.getElementById("groupEmail").className = "form-group has-success";
+    document.getElementById("errorEmail").innerHTML = "";
+  }
+  else {
+    document.getElementById("groupEmail").className = "form-group has-error";
+    document.getElementById("errorEmail").innerHTML = "Please enter valid Email";
+  }
+  if (document.getElementById("CheckboxAccept").checked) {
+    document.getElementById("groupCheckbox").className = "has-success";
+    document.getElementById("errorCheckboxAccept").innerHTML = "";
+  }
+  else {
+    document.getElementById("groupCheckbox").className = "has-error";
+    document.getElementById("errorCheckboxAccept").innerHTML = "Please accept the Terms of Service";
+  }
+  if (document.getElementById("errorName").innerHTML == "" && document.getElementById("errorSurname").innerHTML == "" &&
+      document.getElementById("errorEmail").innerHTML == "" && document.getElementById("errorCheckboxAccept").innerHTML == "") {
+    document.getElementById("submitSuccess").className = "alert alert-success";
+    document.getElementById("submitSuccess").innerHTML = "Thank you! Your form was successfully submitted.";
+  }
 }

3bf3b5ed9e79ea759193f98dfe6161063b450282
eonopchenko <jake.spb@gmail.com>
2017-06-18 18:04:50 +1200

    +Added doubling products ordering ~Fixed add to cart button in description box ~Fixed product display in description box +Added comments

--- js/cart.js
+++ js/cart.js
@@ -1,163 +1,212 @@
-$(document).ready(function()
-{
-	$(document).on("click", ".cart-icon", function(e)
-	{
-		$(this).parent().parent().remove();
-		removeFromCart($(this).attr("name"));
-		e.stopPropagation();
-	});
-	refreshCartButton()
-	if(!isCartEmpty())
-	{
-		getCart();
+/**
+ * Document ready event handler registration
+ * @param  {function} () handler callback
+ */
+$( document ).ready(
+	function() {
+		$( document ).on( "click", ".cart-icon",
+			function( e ) {
+				$( this ).parent().parent().remove();
+				removeFromCart( $( this ).attr( "name" ) );
+				e.stopPropagation();
+			}
+		);
+		refreshCartButton()
+		if ( !isCartEmpty() ) {
+			getCart();
+		}
 	}
-});
+);
 
-function getCart()
-{
+/**
+ * Actual cart table obtainig
+ * @return {String} URI component
+ */
+function getCart() {
 	var total = 0;
 	var cart = getCartElements();
-	$(".cart table tr").remove();
-	$.getJSON( "../catalog.json", function( data ) {
-		$(data.wines).each(function(o, wine)
-		{
-			for(var i=0;i<cart.length;i++)
-			{
-				if(cart[i] != null &&  wine['id'] == cart[i])
-				{
-					$(".cart table").append("<tr><td><img src='images/wines/" + wine['id'] + ".jpg' height='80px' width='32px'></img></td><td><h5>" + wine['name'] + "</h5></td><td><h4> $" + wine['price'] + "</h4></td><td><i class='fa fa-check cart-icon' aria-hidden='true' name='"+ wine['id'] +"'></i></td><td><button class='btn btn-danger' type='button' onClick='removeFromCart(" + wine['id'] + ")'>Remove</button></td></tr>");
-					total = total + parseFloat(wine['price']);
+	$( ".cart table tr" ).remove();
+	$.getJSON( "../catalog.json",
+		function( data ) {
+			$( data.wines ).each(
+				function( o, wine ) {
+					for( var i = 0; i < cart.length; i++ ) {
+						if ( cart[ i ] != null &&  wine[ 'id' ] == cart[ i ] ) {
+							$( ".cart table" ).append( "<tr><td><img src='images/wines/" + wine[ 'id' ] + ".jpg' height='80px' width='32px'></img></td><td><h5>" + wine[ 'name' ] + "</h5></td><td><h4> $" + wine[ 'price' ] + "</h4></td><td><i class='fa fa-check cart-icon' aria-hidden='true' name='"+ wine[ 'id' ] +"'></i></td><td><button class='btn btn-danger' type='button' onClick='removeFromCart(" + wine[ 'id' ] + ")'>Remove</button></td></tr>" );
+							total = total + parseFloat( wine[ 'price' ] );
+						}
+					}
 				}
-			}
-		});
-		$(".cart table")[0].innerHTML += "<tr><td></td><td>Subtotal $" + total + "</td></tr>";
-		refreshCartButton();
-	});
+			);
+			$( ".cart table" )[ 0 ].innerHTML += "<tr><td></td><td>Subtotal $" + total + "</td></tr>";
+			refreshCartButton();
+		}
+	);
 
 	return cart;
 }
 
-function isCartEmpty()
-{
-	if(readCookie("cart") != null && readCookie("cart") != "")
-	{
+/**
+ * Empty cart checking
+ * @return {Boolean} true if cart is empty
+ */
+function isCartEmpty() {
+	if ( readCookie( "cart" ) != null && readCookie( "cart" ) != "" ) {
 		return false;
 	}
 	return true;
 }
 
-function refreshCartButton()
-{
-	$(".cart-button .badge").each(function(i, el)
-	{
-		if(!isCartEmpty())
-		{
+/**
+ * Cart content updating
+ */
+function refreshCartButton() {
+	$( ".cart-button .badge" ).each( function( i, el ) {
+		if ( !isCartEmpty() ) {
 			el.innerHTML = getCartElements().length;
-		} else
-		{
+		}
+		else {
 			el.innerHTML = 0;
-			$(".cart table")[i].innerHTML = "<tr><td>The cart is empty</td></tr>";
+			$( ".cart table" )[ i ].innerHTML = "<tr><td>The cart is empty</td></tr>";
 		}
 	});
 }
 
-function addToCart(wine)
-{
-	if(wine == null || wine < 1)
-	{
+/**
+ * Adding an item to cart
+ * @param {String} wine item to be added
+ */
+function addToCart( wine ) {
+	if ( ( wine == null ) || ( wine < 1 ) ) {
 		return;
 	}
-	if(readCookie("cart") == null)
-	{
-		var cart = [];
-		cart.push(wine);
-		createCookie("cart", cart, 1);
-	}
-	else
-	{
+
+	var cart = [];
+	if ( readCookie( "cart" ) != null) {
 		var cart = getCartElements();
-		
-		var exist = false;
-		cart.forEach(function(c)
-		{
-			if(wine == c)
-			{
-				exist = true;
-				return;
-			}
-		})
-		
-		if(!exist)
-		{
-			cart.push(wine);
-			createCookie("cart", cart, 1);
-		}
 	}
+
+	cart.push( wine );
+	createCookie( "cart", cart, 1 );
+
 	getCart();
 }
 
-function getCartElements()
-{
-	var cart = readCookie("cart");
-	cart = trimChar(cart, ",");
-	cart = cart.split(",");
-	if(cart.length < 1 || (cart.length > 1 && cart[0] == "")) return [];
+/**
+ * Obtaining of cart content from cookies
+ * @return {String} URI component
+ */
+function getCartElements() {
+	var cart = readCookie( "cart" );
+	cart = trimChar( cart, "," );
+	cart = cart.split( "," );
+
+	if ( ( cart.length < 1 ) || ( ( cart.length > 1 ) && ( cart[ 0 ] == "" ) ) ) {
+		return [];
+	}
+
 	return cart;
 }
 
-function removeFromCart(wine)
-{
-	var cart = readCookie("cart").split(",");
+/**
+ * Deletion of an item from cart
+ * @param  {String} wine Wine ID
+ */
+function removeFromCart( wine ) {
+	var cart = readCookie( "cart" ).split( "," );
 	var newCart = [];
-	for(var i=0;i<cart.length;i++)
-	{
-		var wineNo = cart[i];
-		if(wineNo != null)
-		{
-			if(wineNo != wine)
-			{
-				newCart.push(wineNo);
+	var deleted = false;
+	for( var i = 0; i < cart.length; i++ ) {
+		var wineNo = cart[ i ];
+		if ( wineNo != null ) {
+			if ( ( wineNo == wine ) && !deleted ) {
+				deleted = true;
+			}
+			else {
+				newCart.push( wineNo );
 			}
 		}
 	}
-	createCookie("cart", newCart, 1);
+	createCookie( "cart", newCart, 1 );
 	refreshCartButton();
 	getCart();
 }
 
-function trimChar(string, charToRemove) {
-    if (charToRemove === "") return string;
-    var start = 0, end = string.length-1;
-    while (string.charAt(end) == charToRemove) end--;
-    if (end === -1) return "";
-    while (string.charAt(start) == charToRemove) start++;
-    return string.slice(start, end+1);
+/**
+ * String characters trim
+ * @param  {String} string	     input string
+ * @param  {String} charToRemove character to remove
+ * @return {String}			         result string
+ */
+function trimChar(string, charToRemove)
+{
+	if ( charToRemove === "" ) {
+		return string;
+	}
+
+	var start = 0, end = string.length - 1;
+	while ( string.charAt( end ) == charToRemove ) {
+		end--;
+	}
+	if ( end === -1 ) {
+		return "";
+	}
+
+	while ( string.charAt( start ) == charToRemove ) {
+		start++;
+	}
+
+	return string.slice( start, end + 1 );
 }
 
-function createCookie(name, value, days) {
-    var expires;
+/**
+ * Syncronization cookies with cart
+ * @param  {String} name  name of cookie
+ * @param  {String} value values to be saved
+ * @param  {Number} days  days before disposal
+ * @return {[type]}	   [description]
+ */
+function createCookie( name, value, days ) {
 
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toGMTString();
-    } else {
-        expires = "";
-    }
-    document.cookie = encodeURIComponent(name) + "=" + encodeURIComponent(value) + expires + "; path=/";
+	var expires = "";
+
+	if ( days ) {
+		var date = new Date();
+		date.setTime( date.getTime() + ( days * 24 * 60 * 60 * 1000 ) );
+		expires = "; expires=" + date.toGMTString();
+	}
+
+	document.cookie = encodeURIComponent( name ) + "=" + encodeURIComponent( value ) + expires + "; path=/";
 }
 
-function readCookie(name) {
-    var nameEQ = encodeURIComponent(name) + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return decodeURIComponent(c.substring(nameEQ.length, c.length));
-    }
-    return null;
+/**
+ * Obtaining data from cookies
+ * @param  {String} name cookie name
+ * @return {String}      URI component
+ */
+function readCookie( name ) {
+	var nameEQ = encodeURIComponent( name ) + "=";
+	var ca = document.cookie.split( ';' );
+
+	for( var i = 0; i < ca.length; i++ ) {
+		var c = ca[ i ];
+
+		while ( c.charAt( 0 ) === ' ' ) {
+			c = c.substring( 1, c.length );
+		}
+
+		if ( c.indexOf( nameEQ ) === 0 ) {
+			return decodeURIComponent( c.substring( nameEQ.length, c.length ) );
+		}
+	}
+
+	return null;
 }
 
-function eraseCookie(name) {
-    createCookie(name, "", -1);
+/**
+ * Cookie deletion
+ * @param  {String} name cookie name
+ */
+function eraseCookie( name ) {
+	createCookie( name, "", -1 );
 }
--- js/site.js
+++ js/site.js
@@ -1,150 +1,153 @@
-// JavaScript Document
-//Home and product page
+/**
+ * Product page initialization
+ */
 function init() {
-	$.getJSON( "../catalog.json", function( data ) {
-	var items = [];
-	var categories = [];
-	$.each( data.wines, function( i, wine ) {
-		//alert(wine['name']);
-		var cat = wine['category'];
-		cat = cat.replace(/ /g, '_');
-		items.push(generateProductDiv(wine));
-		var current = "<li name='" + cat + "'>" + wine['category'] + "</li>";
-		if($.inArray(current, categories) === -1)
-		{
-			categories.push(current);
-		}
-	});
-	items.push("<div class='clear'></div>");
-	$(".products")[0].innerHTML = "";
-	$(".categories")[0].innerHTML = "";
-	for(var i=0; i<categories.length; i++)
-	{
-		$(".categories").get(0).innerHTML += categories[i];
-	}
-	for(var i=0; i<items.length; i++)
-	{
-		$(".products").get(0).innerHTML += items[i];
-	}
-	});
-	$(document).on("click", ".infobox button", function(e)
-	{
-		addToCart($(this).attr("name"));
-		e.stopPropagation();
-	});
-}
+	$.getJSON( "../catalog.json",
+		function( data ) {
+			var items = [];
+			var categories = [];
 
-function generateProductDiv(wine)
-{
-	var cat = wine['category'];
-	cat = cat.replace(/ /g, '_');
-	return "<div class='lightbox infobox " + cat + "'><img src='images/wines/" + wine['id'] + ".jpg'></img><h5>" + wine['name'] + "</h5><center><h4>$" + wine['price'] + "</h4></center><button type='button' class='btn btn-default' aria-haspopup='true' name='" + wine['id'] + "' aria-expanded='false'>Add to cart <span class='glyphicon glyphicon-shopping-cart'></span></button><div class='lightbox-more'><div class='col-sm-14 text-center'><img class='img-responsive text-center center-block' src='images/wines/" + wine['id'] + ".jpg' /></div><br />			<div class='text-center'> <h2>" + wine['name'] + "</h2><p>Description: " + wine['desc'] + "</p><p>Category: " + wine['category'] + "</p><h4>Price: $" + wine['price'] + "</h4></div><button style='position:absolute;bottom:10px;right:10px;' class='btn btn-danger' type='button'>Add to cart</button></div></div>";
-}
+			$.each( data.wines,
+				function( i, wine ) {
+					var cat = wine[ 'category' ];
+					cat = cat.replace( / /g, '_' );
+					items.push( generateProductDiv( wine ) );
+					var current = "<li name='" + cat + "'>" + wine[ 'category' ] + "</li>";
+					if( $.inArray( current, categories ) === -1 ) {
+						categories.push( current );
+					}
+				}
+			);
 
-function getwine(id)
-{
-	$.getJSON( "../catalog.json", function( data ) {
-		$.each( data.wines, function( i, wine )
-		{
-			if(id == wine['id'])
-			{
-				return {'id' : wine['id']};
+			items.push( "<div class='clear'></div>" );
+			$( ".products" )[ 0 ].innerHTML = "";
+			$( ".categories" )[ 0 ].innerHTML = "";
+			for( var i = 0; i < categories.length; i++ ) {
+				$( ".categories" ).get( 0 ).innerHTML += categories[ i ];
 			}
-		});
-	});
-	return null;
+			for( var i = 0; i < items.length; i++ ) {
+				$( ".products" ).get( 0 ).innerHTML += items[ i ];
+			}
+		}
+	);
+	$( document ).on( "click", ".infobox button",
+		function( e ) {
+			addToCart( $( this ).attr( "name" ) );
+			e.stopPropagation();
+		}
+	);
 }
 
-function goTop(elemt)
-{
-	$('html, body').animate({
-		scrollTop: "0px"
-	}, 800);
+/**
+ * Product div block generation
+ * @param  {String} wine [description]
+ * @return {String}      div block markup
+ */
+function generateProductDiv( wine ) {
+	var cat = wine[ 'category' ];
+	cat = cat.replace( / /g, '_' );
+	return "<div class='lightbox infobox " + cat + "'><img src='images/wines/" + wine[ 'id' ] + ".jpg'></img><h5>" + wine[ 'name' ] + "</h5><center><h4>$" + wine[ 'price' ] + "</h4></center><button type='button' class='btn btn-default' aria-haspopup='true' name='" + wine[ 'id' ] + "' aria-expanded='false'>Add to cart <span class='glyphicon glyphicon-shopping-cart'></span></button><div class='lightbox-more'><div class='col-sm-14 text-center'><img class='img-responsive text-center center-block' src='images/wines/" + wine[ 'id' ] + ".jpg' /></div><br />			<div class='text-center'> <h2>" + wine[ 'name' ] + "</h2><p>Description: " + wine[ 'desc' ] + "</p><p>Category: " + wine[ 'category' ] + "</p><h4>Price: $" + wine[ 'price' ] + "</h4></div><button style='position:absolute;bottom:10px;right:10px;' class='btn btn-danger' type='button' onClick='addToCart(" + wine[ 'id' ] + ")'>Add to cart</button></div></div>";
 }
 
+/**
+ * Deals block initialization
+ */
 function initDeal()
 {
-	$.getJSON( "../catalog.json", function( data ) {
-	var items = [];
-	$.each( data.deal, function( i, wine ) {
-		var w = data.wines[wine['id']];
-		var cat = w['category'];
-		cat = cat.replace(/ /g, '_');
-		items.push(generateProductDiv(w));
-		var current = "<li name='" + cat + "'>" + w['category'] + "</li>";
-	});
-	$(".deal")[0].innerHTML = "";
-	for(var i=0; i<items.length; i++)
-	{
-		$(".deal").get(0).innerHTML += items[i];
-	}
-	});
-}
-
-function initPopular()
-{
-	$.getJSON( "../catalog.json", function( data ) {
-	var items = [];
-	$.each( data.popular, function( i, wine ) {
-		var w = data.wines[wine['id']];
-		var cat = w['category'];
-		cat = cat.replace(/ /g, '_');
-		items.push(generateProductDiv(w));
-		var current = "<li name='" + cat + "'>" + w['category'] + "</li>";
-	});
-	$(".popular")[0].innerHTML = "";
-	for(var i=0; i<items.length; i++)
-	{
-		$(".popular").get(0).innerHTML += items[i];
-	}
-	});
-}
-
-function initHome()
-{
-	$(document).on("click", ".infobox button", function(e)
-	{
-		addToCart($(this).attr("name"));
-		e.stopPropagation();
-	});
-}
-
-$( document ).ready(function() {
-
-	$("body")[0].innerHTML += "<div class='lightbox-lightbox'></div>";
-
-	$(document).on('click', ".lightbox", function(event) {
-		var lightboxMore = $(this).find(".lightbox-more");
-		if(lightboxMore.length > 0)
-		{
-			lightboxMore.css({"display" : "block"});
-		} else
-		{
-			var newEl = $(this).find("img").clone(true).addClass("lightbox-imageonly").appendTo($(this));
+	$.getJSON( "../catalog.json",
+		function( data ) {
+			var items = [];
+			$.each( data.deal,
+				function( i, wine ) {
+					var w = data.wines[ wine[ 'id' ] ];
+					var cat = w[ 'category' ];
+					cat = cat.replace( / /g, '_' );
+					items.push( generateProductDiv( w ) );
+					var current = "<li name='" + cat + "'>" + w[ 'category' ] + "</li>";
+				}
+			);
+			$( ".deal" )[ 0 ].innerHTML = "";
+			for( var i = 0; i < items.length; i++ ) {
+				$( ".deal" ).get( 0 ).innerHTML += items[ i ];
+			}
 		}
-		$("body").find(".lightbox-lightbox").css({"display" : "block"});
-		event.stopPropagation();
-	});
+	);
+}
 
-	$(document).on('click', '.lightbox-lightbox', function()
-	{
-		$(document).find(".lightbox-more").css({"display" : "none"});
-		$(this).css({"display" : "none"});
-	});
-	//Toggle which products are shown
-	$(document).on('click', '.categories li', function(event)
-	{
-		var name = $(this).attr("name");
-		if($(this).css("color").replace(/\D+/g, '') === '128128128')
-		{
-			$(this).css({"color" : "black"});
-			$(".products").find("." + name).css({"display" : "block"});
-		} else
-		{
-			$(".products").find("." + name).css({"display" : "none"});
-			$(this).css({"color" : "gray"});
+/**
+ * Popular products block initialization
+ */
+function initPopular() {
+	$.getJSON( "../catalog.json", function( data ) {
+		var items = [];
+		$.each( data.popular,
+			function( i, wine ) {
+				var w = data.wines[wine[ 'id' ]];
+				var cat = w[ 'category' ];
+				cat = cat.replace( / /g, '_' );
+				items.push( generateProductDiv( w ) );
+				var current = "<li name='" + cat + "'>" + w[ 'category' ] + "</li>";
+			}
+		);
+		$( ".popular" )[ 0 ].innerHTML = "";
+		for( var i = 0; i<items.length; i++ ) {
+			$( ".popular" ).get( 0 ).innerHTML += items[ i ];
 		}
-		event.stopPropagation();
 	});
+}
 
-});
+/**
+ * Home page initialization
+ */
+function initHome() {
+	$( document ).on( "click", ".infobox button",
+		function( e ) {
+			addToCart($( this ).attr( "name" ));
+			e.stopPropagation();
+		}
+	);
+}
+
+/**
+ * Document ready event handler registration
+ * @param  {function} () handler callback
+ */
+$( document ).ready(
+	function() {
+		$( "body" )[ 0 ].innerHTML += "<div class='lightbox-lightbox'></div>";
+
+		$( document ).on( 'click', ".lightbox",
+			function( event ) {
+				var lightboxMore = $(this).find(".lightbox-more");
+				if( lightboxMore.length > 0 ) {
+					lightboxMore.css( { "display" : "block" } );
+				} else {
+					var newEl = $( this ).find( "img" ).clone( true ).addClass( "lightbox-imageonly" ).appendTo( $( this ) );
+				}
+				$( "body" ).find( ".lightbox-lightbox" ).css( { "display" : "block" } );
+				event.stopPropagation();
+			}
+		);
+
+		$(document).on('click', '.lightbox-lightbox',
+			function() {
+				$( document ).find( ".lightbox-more" ).css( { "display" : "none" } );
+				$( this ).css( { "display" : "none" } );
+			}
+		);
+
+		$( document ).on( 'click', '.categories li',
+			function( event ) {
+				var name = $( this ).attr( "name" );
+				if( $( this ).css( "color" ).replace( /\D+/g, '' ) === '128128128' ) {
+					$( this ).css( { "color" : "black" } );
+					$( ".products" ).find( "." + name ).css( { "display" : "block" } );
+				} else {
+					$( ".products" ).find( "." + name ).css( { "display" : "none" } );
+					$( this ).css( { "color" : "gray" } );
+				}
+				event.stopPropagation();
+			}
+		);
+	}
+);
